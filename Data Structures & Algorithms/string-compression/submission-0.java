88352edeb3d18af4e74d6e87a79a1169e86e1248
Vinitha Y <vinithayuvaraja2005@gmail.com>
2026-06-29 16:28:39 +0530

    Add: string-compression - submission-0

---
+++ Data Structures & Algorithms/string-compression/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb=new StringBuilder();
+        int count=1;
+        for(int i=1;i<chars.length;i++){
+            if(chars[i-1]==chars[i]){
+                count++;
+            }
+            else{
+                if(count==1){
+                    sb.append(chars[i-1]);
+                }
+                else{
+                    sb.append(chars[i-1]).append(count+"");
+                    count=1;
+                }
+            }
+        }
+        if(count==1){
+            sb.append(chars[chars.length-1]);
+        }else{
+             sb.append(chars[chars.length-1]).append(count+"");
+        }
+    
+         for(int i=0;i<sb.length();i++){
+            chars[i]=sb.charAt(i);
+         }   
+            
+      return sb.length();
+        
+
+        
+    }
+}
